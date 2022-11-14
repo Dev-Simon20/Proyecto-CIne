03effe467aa26a363471c2ea54e23e138b850583
jean <jeanpierks6@gmail.com>
2022-11-14 09:07:22 -0500

    version2

--- src/ProyectoCine/Pelicula.java
+++ src/ProyectoCine/Pelicula.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ProyectoCine;
 
-/**
- *
- * @author jean
- */
 public class Pelicula {
+    String titulo;
+    String duracion;
+    int edad;
+    String director;
     
 }
